3a7c455f652a3e647c706bd67c4479667738d051
Paulina <paulina.borowiecka554@gmail.com>
2025-01-25 23:38:50 +0100

    Converted class components to the functional components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,28 +1,24 @@
 import React from "react";
 
-class TodoItem extends React.Component {
-  render() {
-    const completedStyle = {
-      fontStyle: "italic",
-      color: "#d35e0f",
-      opacity: 0.4,
-      textDecoration: "line-through"
-    };
+export default function TodoItem({ todo, handleChangeProps, deleteTodoProps }) {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#d35e0f",
+    opacity: 0.4,
+    textDecoration: "line-through"
+  };
 
-    const { completed, id, title } = this.props.todo;
+  const { completed, id, title } = todo;
 
-    return (
-      <li className="todo-item">
-        <input
-          type="checkbox"
-          checked={completed}
-          onChange={() => this.props.handleChangeProps(id)}
-        />
-        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-        <span style={completed ? completedStyle : null}>{title}</span>
-      </li>
-    );
-  }
+  return (
+    <li className="todo-item">
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => handleChangeProps(id)}
+      />
+      <button onClick={() => deleteTodoProps(id)}>Delete</button>
+      <span style={completed ? completedStyle : null}>{title}</span>
+    </li>
+  );
 }
-
-export default TodoItem;
